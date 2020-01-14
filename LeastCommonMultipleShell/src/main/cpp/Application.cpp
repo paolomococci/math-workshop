dfb154a71a38af9bd65b1a9411f81a0854f2bebc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-01-14 20:36:28 +0100

    Create Application.cpp

---
+++ LeastCommonMultipleShell/src/main/cpp/Application.cpp
@@ -0,0 +1,32 @@
+/*
+ * Application.cpp
+ *
+ * Copyright (C) 2020 paolo mococci
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include "Application.h"
+
+std::string paolomococci::Hello::getHello() {
+    return "Hi!";
+}
+
+int main () {
+    paolomococci::Hello hello;
+    std::cout << hello.getHello() << std::endl;
+    std::exit(EXIT_SUCCESS);
+}
