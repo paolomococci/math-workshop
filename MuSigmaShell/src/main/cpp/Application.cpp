8f6d7a7fb77150632d99edbeee1035137b555900
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-01-21 23:54:48 +0100

    Update Application.cpp

--- MuSigmaShell/src/main/cpp/Application.cpp
+++ MuSigmaShell/src/main/cpp/Application.cpp
@@ -24,10 +24,6 @@ int main(int argc, char** argv) {
 	paolomococci::Main main;
 	main.setArgIndex(argc);
 	main.setArgVector(argv);
-	std::cout
-		<< main.execute()
-		<< " boolean return value of main.execute()"
-		<< std::endl;
 	//if (!main.execute()) std::exit(EXIT_FAILURE);
 	std::exit(EXIT_SUCCESS);
 }
